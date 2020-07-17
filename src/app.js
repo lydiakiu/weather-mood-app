7d77d784df8e04df9a40fdca1afbd81c7b7452eb
Lydia K <64658106+lydiakiu@users.noreply.github.com>
2020-07-17 16:04:14 -0700

    celcius fahrenheit conversion
    removed default image
    added celcius and fahrenheir conversions

--- src/app.js
+++ src/app.js
@@ -32,13 +32,17 @@ function displayTemp(response) {
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  celciusTemperature = response.data.main.temp;
+  minCelciusTemperature = response.data.main.temp_min;
+  maxCelciusTemperature = response.data.main.temp_max;
+
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
-  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(minCelciusTemperature) + "°C";
+  maxTempElement.innerHTML = Math.round(maxCelciusTemperature) + "°C";
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -59,7 +63,48 @@ function handleSubmit(event) {
   search(cityElement.value);
 }
 
+function displayFahTemp(event) {
+  event.preventDefault();
+  let fahrenheit = (celciusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  let maxFahrenheit = (maxCelciusTemperature * 9) / 5 + 32;
+  let maxTempElement = document.querySelector("#maxtemp");
+  let minFahrenheit = (minCelciusTemperature * 9) / 5 + 32;
+  let minTempElement = document.querySelector("#mintemp");
+
+  celciusLink.classList.remove("inactive");
+  fahrenheitLink.classList.add("inactive");
+
+  temperatureElement.innerHTML = Math.round(fahrenheit);
+  maxTempElement.innerHTML = Math.round(maxFahrenheit) + "°F";
+  minTempElement.innerHTML = Math.round(minFahrenheit) + "°F";
+}
+
+function displayCelTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let maxTempElement = document.querySelector("#maxtemp");
+  let minTempElement = document.querySelector("#mintemp");
+
+  celciusLink.classList.add("inactive");
+  fahrenheitLink.classList.remove("inactive");
+
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+  maxTempElement.innerHTML = Math.round(maxCelciusTemperature) + "°C";
+  minTempElement.innerHTML = Math.round(minCelciusTemperature) + "°C";
+}
+
+let celciusTemperature = null;
+let maxCelciusTemperature = null;
+let minCelciusTemperature = null;
+
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", handleSubmit);
 
-search("vancouver");
+search("munich");
+
+let fahrenheitLink = document.querySelector("#fah-link");
+fahrenheitLink.addEventListener("click", displayFahTemp);
+
+let celciusLink = document.querySelector("#cel-link");
+celciusLink.addEventListener("click", displayCelTemp);
