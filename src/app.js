db33a3a369982440d859c55d66c010ea521b1cd7
Lydia K <64658106+lydiakiu@users.noreply.github.com>
2020-07-17 11:40:21 -0700

    Weather icons
    Linked weather icons to open weather

--- src/app.js
+++ src/app.js
@@ -28,15 +28,21 @@ function displayTemp(response) {
   let conditionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "57a751478ef508961135e0d25d48186e";
-let city = "tokyo";
+let city = "denver";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
